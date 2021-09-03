518f7012b056178558afb6d197698468bfb4d276
Septiyadi <septiyadi@mhs.pelitabangsa.ac.id>
2021-09-03 16:02:23 +0700

    Add project dart programming

---
+++ com/dart/beginner/class/helloworld.dart
@@ -0,0 +1,26 @@
+void main() {
+  /*
+  * Tipedata
+  * String, Integer, Double, Float, Char, Boolean
+  */
+  String namaLengkap = "Asep Septiadi";
+  int umurMahasiswa = 24;
+  double tinggiBadan = 167.4;
+
+  /*
+  * Tipedata var
+  * Apabila kita membuat variabel menggunakan kata kunci var, maka variabel tersebut akan bisa diisi dengan tipe data apapun.
+  */
+  var namaSiswa = "Dian Indriwati";
+  var umurSiswa = 15;
+
+  /*
+  * Menampikan String Hello World
+  */
+  print('Hello world');
+
+  /*
+  * Memanggil variable ke output
+  */
+  print('Menampilkan variable $tinggiBadan');
+}
